b22973aebaaa0df7537ef9da07f718a653e279a9
VinayJose456 <75767945+VinayJose456@users.noreply.github.com>
2021-04-05 20:54:24 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var wagon1,wagon2;
+var obstacles = [];
+
+const NUM_OBSTACLES = 40;
+function setup(){
+  createCanvas(400,400);
+ wagon1 = new wagon(width*0.33);
+ wagon2 = new wagon(width*0.73); 
+ for(var i=0;i<NUM_OBSTACLES;i++){
+   obstacles.push(new obstacle());
+ }
+}
+
+function draw(){
+  background(0);
+  wagon1.update();
+  wagon2.update();
+  wagon1.display();
+  wagon2.display();
+  updateObstacle();
+}
+
+function updateObstacle(){
+  for(var i=0;i<obstacles.length;i++){
+    obstacles[i].update();
+    obstacles[i].display();
+    if(obstacles[i].obstacleHitWagon(wagon1)){
+      wagon1.respawn();
+    }
+    else if(obstacles[i].obstacleHitWagon(wagon2)){
+      wagon2.respawn();
+    }
+  }
+}
+function keyPressed(){
+  if(keyCode==UP_ARROW){
+    wagon1.isUp=true;
+    wagon.isDown=false;
+  }
+  else if(keyCode==DOWN_ARROW){
+    wagon1.isDown=true;
+    wagon1.isUp=false;
+  }
+  if(keyCode==87){
+    wagon2.isUp=true;
+    wagon2.isDown=false;
+  }
+  else if(keyCode==83){
+    wagon2.isDown=true;
+    wagon2.isUp=false;
+  }
+}
+
+function keyReleased(){
+  if(keyCode==UP_ARROW){
+    wagon1.isUp=false;
+  }
+  else if(keyCode==DOWN_ARROW){
+    wagon1.isDown=false;
+  }
+  if(keyCode==87){
+    wagon2.isUp=false
+  }
+  else if(keyCode==83){
+    wagon2.isDown=false;
+  }
+}
